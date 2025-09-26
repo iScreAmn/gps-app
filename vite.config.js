90ab6e5af49f1c3328634e1dbced6e7ebce625d7
Dimitri. J <jmukhadze.dimitri@gmail.com>
2025-09-26 16:43:21 +0400

    Add GitHub Actions deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/gps-app/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  },
   server: {
     host: true,
     port: 5173,
