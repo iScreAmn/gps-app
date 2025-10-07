32ff78e612cb92ec43d7907991348d6cb1a72e7b
Dimitri. J <jmukhadze.dimitri@gmail.com>
2025-10-08 00:40:45 +0400

    Container width fixes

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
 
   return (
     <section className="hero">
-      <div className="hero-container">
+      <div className="container">
         <div className="hero-layout">
           {/* Left Sidebar - Category List */}
           <div className="hero-sidebar hero-sidebar-left">
