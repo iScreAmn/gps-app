3c81c9932f6a71f98cb62915253ff818c61313fa
Dimitri. J <jmukhadze.dimitri@gmail.com>
2025-10-16 23:10:54 +0400

    Removed unused mode parameter from vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/gps-app/' : '/',
+export default defineConfig(() => ({
+  base: '/test/',
   plugins: [react()],
   build: {
     outDir: 'dist',
