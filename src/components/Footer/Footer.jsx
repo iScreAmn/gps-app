f59fe4c9e1eb8b0e15e643817653b772306ede24
Dimitri. J <jmukhadze.dimitri@gmail.com>
2025-11-07 13:20:52 +0400

    Add social media icons-links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,22 +1,21 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 import { useLanguage } from "../../hooks/useLanguage";
-import { useTheme } from '../../contexts/ThemeContext';
-import { mainLogo, mainLogoWhite } from '../../assets/images';
-import contactsData from '../../data/contactsData';
-import { FaPhoneAlt } from "react-icons/fa";
+import { useTheme } from "../../contexts/ThemeContext";
+import { mainLogo, mainLogoWhite } from "../../assets/images";
+import contactsData from "../../data/contactsData";
+import { FaPhoneAlt, FaFacebookSquare, FaInstagram, FaLinkedinIn, FaYoutube } from "react-icons/fa";
 import { MdOutlineMail, MdLocationPin } from "react-icons/md";
-
-
-import './Footer.css';
+import { AiFillTikTok } from "react-icons/ai";
+import "./Footer.css";
 
 const Footer = () => {
   const { language, t } = useLanguage();
   const { theme } = useTheme();
   const currentYear = new Date().getFullYear();
-  
+
   // Determine logo based on theme
-  const currentLogo = theme === 'dark' ? mainLogoWhite : mainLogo;
+  const currentLogo = theme === "dark" ? mainLogoWhite : mainLogo;
 
   return (
     <footer className="footer">
@@ -24,19 +23,17 @@ const Footer = () => {
         <div className="footer-content">
           <div className="footer-section">
             <Link to={`/${language}`} className="logo-footer">
-              <img 
-                src={currentLogo} 
-                alt="Georgian GPS Logo" 
+              <img
+                src={currentLogo}
+                alt="Georgian GPS Logo"
                 className="footer-logo-image"
               />
             </Link>
-            <p className="footer-description">
-              {t('footer.description')}
-            </p>
+            <p className="footer-description">{t("footer.description")}</p>
           </div>
-          
+
           <div className="footer-section">
-            <h4>{t('footer.contact')}</h4>
+            <h4>{t("footer.contact")}</h4>
             <a
               href={contactsData.phone.href}
               className="footer-contact-item footer-contact-link"
@@ -53,19 +50,41 @@ const Footer = () => {
             </a>
             <div className="footer-contact-item">
               <MdLocationPin />
-              <span>{t('contacts.tbilisi_georgia')}</span>
+              <span>{t("contacts.tbilisi_georgia")}</span>
             </div>
           </div>
         </div>
-        
+
         <div className="footer-bottom">
-          <p>&copy; {currentYear} Georgian Polygraph Services. {t('footer.all_rights')}.</p>
-          <Link
-            to={`/${language}/privacy-policy`}
-            className="footer-bottom-link"
-          >
-            {t('footer.privacy_policy')}
-          </Link>
+          <div className="copyright-info">
+            <p>
+              &copy; {currentYear} Georgian Polygraph Services.{" "}
+              {t("footer.all_rights")}.
+            </p>
+            <Link
+              to={`/${language}/privacy-policy`}
+              className="footer-bottom-link"
+            >
+              {t("footer.privacy_policy")}
+            </Link>
+          </div>
+          <div className="copyright-socials">
+            <a href="https://facebook.com/GeorgianPolygraphServices" target="_blank" className="copyright-social-link">
+              <FaFacebookSquare />
+            </a>
+            <a href="https://instagram.com/geopolygraph" target="_blank" className="copyright-social-link">
+              <FaInstagram />
+            </a>
+            <a href="https://linkedin.com/company/georgian-polygraph-services" target="_blank" className="copyright-social-link">
+              <FaLinkedinIn />
+            </a>
+            <a href="https://tiktok.com/@georgian.polygrap" target="_blank" className="copyright-social-link">
+              <AiFillTikTok />
+            </a>
+            <a href="https://www.youtube.com/@geopolser" target="_blank" className="copyright-social-link">
+              <FaYoutube />
+            </a>
+          </div>
         </div>
       </div>
     </footer>
