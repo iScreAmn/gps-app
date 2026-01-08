9ff783465740ed7effc61d02c983e4fdcb4edf2a
Dimitri. J <jmukhadze.dimitri@gmail.com>
2026-01-08 12:23:21 +0400

    Updated info in special discounts

--- src/components/SpecialDiscounts/SpecialDiscounts.jsx
+++ src/components/SpecialDiscounts/SpecialDiscounts.jsx
@@ -1,13 +1,10 @@
 import React, { useState } from 'react'
-import { proModel } from '../../assets/images'
+import { nocaiArt } from '../../assets/images'
 import DiscountModal from '../widgets/Modals/Discount'
 import './SpecialDiscounts.css'
 
 const SpecialDiscounts = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-
-  // Установите дату окончания акции
-  const targetDate = new Date('2025-12-31T23:59:59');
 
   const handleOpenModal = () => {
     setIsModalOpen(true);
@@ -24,21 +21,23 @@ const SpecialDiscounts = () => {
         <div className="discounts__wrapper">
           <div className="discounts__text">
             <h3 className="discounts__title">მეტი ბეჭდვა-მეტი სარგებელი!</h3>
-            <p className="discounts__description">დეკემბრის თვის განსაკუთრებული შეთავაზება! მხოლოდ PRO მოდელის პრინტერის Click სერვისით სარგებლობისას, დაბეჭდეთ  1000 ლარზე მეტი ღირებულების A4 გვერდი და მიიღეთ 10%-იანი ფასდაკლება.
+            <p className="discounts__description">დეკემბრის თვის განსაკუთრებული შეთავაზება Nocai-ის UV პრინტერზე, დაგვიკავშირდით და მიიღეთ ფასდაკლება
             </p>
             <button className="discounts__button" onClick={handleOpenModal}>
               შეთავაზების მიღება
             </button>
           </div>
-          <img className="discounts__image" src={proModel} alt="Discount" />
+          <img className="discounts__image" src={nocaiArt} alt="Discount" />
         </div>
       </div>
 
       <DiscountModal
         isOpen={isModalOpen}
         onClose={handleCloseModal}
-        targetDate={targetDate}
-        description="დეკემბრის თვის განსაკუთრებული შეთავაზება! დარჩენილია:"
+        description="დეკემბრის თვის განსაკუთრებული შეთავაზება!"
+        countdown={{
+          targetDate: "2025-12-31T23:59:59"
+        }}
       />
     </section>
   )
