e65e23c376e1a9f86770399e5e64ab29f3130336
Dimitri. J <jmukhadze.dimitri@gmail.com>
2026-03-12 14:00:04 +0400

    Updated styles of deals

--- src/components/BestDeals/BestDeals.jsx
+++ src/components/BestDeals/BestDeals.jsx
@@ -5,10 +5,10 @@ import tonnerBg from '../../assets/images/icons/tonner-bg.webp';
 import './BestDeals.css';
 
 const BestDeals = () => {
-  const { t } = useLanguage();
+  const { language, t } = useLanguage();
 
   return (
-    <div className="best-deals">
+    <Link to={`/${language}/catalog/supplies`} className="best-deals">
       <div className="deals-header">
         <h3 className="deals-title">{t('deals.title')}</h3>
         <p className="deals-subtitle none">{t('deals.subtitle')}</p>
@@ -18,12 +18,7 @@ const BestDeals = () => {
         <img src={tonnerBg} alt="Toner cartridge" className="deals-image" />
       </div>
       
-      <div className="deals-cta none">
-        <Link to="/catalog/supplies" className="deals-button">
-          {t('deals.cta')}
-        </Link>
-      </div>
-    </div>
+    </Link>
   );
 };
 
